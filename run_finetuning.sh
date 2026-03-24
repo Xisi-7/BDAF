e0902a58dfe9152b5f0370a012e8e01e2b682a39
Xisi-7 <130621401+Xisi-7@users.noreply.github.com>
2026-03-24 16:38:19 +0800

    Update run_finetuning.sh

--- run_finetuning.sh
+++ run_finetuning.sh
@@ -1,137 +1,17 @@
 # ===================== 批量运行脚本 =====================
 # 运行对抗微调脚本
 echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset animals10  --victim ressl --gpu 0
+python adversarial_fine-tuning.py --dataset animals10  --victim byol --gpu 0
 
 # 运行标准微调脚本
 echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset animals10 --victim ressl --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset cifar10  --victim ressl --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset cifar10 --victim ressl --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset gtsrb  --victim ressl --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset gtsrb --victim ressl --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset stl10  --victim ressl --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset stl10 --victim ressl --gpu 0
+python standard_finetuning.py --dataset animals10 --victim byol --gpu 0
 
 
 
 
 
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset animals10  --victim swav --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset animals10 --victim swav --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset cifar10  --victim swav --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset cifar10 --victim swav --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset gtsrb  --victim swav --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset gtsrb --victim swav --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset stl10  --victim swav --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset stl10 --victim swav --gpu 0
 
 
-
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset animals10  --victim wmse --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset animals10 --victim wmse --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset cifar10  --victim wmse --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset cifar10 --victim wmse --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset gtsrb  --victim wmse --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset gtsrb --victim wmse --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset stl10  --victim wmse --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset stl10 --victim wmse --gpu 0
-
-
-
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset gtsrb  --victim nnclr --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset gtsrb --victim nnclr --gpu 0
-
-# ===================== 批量运行脚本 =====================
-# 运行对抗微调脚本
-echo -e "\nRunning adversarial_fine-tuning.py ..."
-python adversarial_fine-tuning.py --dataset stl10  --victim nnclr --gpu 0
-
-# 运行标准微调脚本
-echo -e "\nRunning standard_finetuning.py ..."
-python standard_finetuning.py --dataset stl10 --victim nnclr --gpu 0
 
 
